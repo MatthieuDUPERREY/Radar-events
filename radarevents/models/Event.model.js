00c586749434847c89ab38775b24975e55e24ae0
Matthieu Duperrey <matth.duperrey@gmail.com>
2022-07-18 14:06:54 +0200

    auth routes done

--- radarevents/models/Event.model.js
+++ radarevents/models/Event.model.js
@@ -3,11 +3,20 @@ const { Schema, model } = require("mongoose");
 
 const eventSchema = new Schema(
   {
-    title: String,
-    location: String,
-    date: Date,
+    title: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    dateAndTime: {
+      type: Date,
+      required: true
+    },
     category: String,
-    description: String,    
+    description: String,
     },
   {
     timestamps: true,
--- radarevents/models/User.model.js
+++ radarevents/models/User.model.js
@@ -1,9 +1,12 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
+      required: [true, 'Email is required.'],
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
       unique: true,
+      lowercase: true,
       trim: true,
     },
     passwordHash: {
